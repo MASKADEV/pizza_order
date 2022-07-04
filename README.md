28cc96b41e48efda5a647c18fff0fa9afab05485
MASKADEV <sidkiyouness@gmail.com>
2022-07-04 12:27:42 -0700

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+## Setup Next.JS with TypeScript, Prettier, ESLint and Husky
+
+`Ready To Clone Repo and start your journey!`
+
+# crud-nextjs-mongodb
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div className="text-4xl text-red-400">MASKA</div>;
+};
+
+export default Home;
